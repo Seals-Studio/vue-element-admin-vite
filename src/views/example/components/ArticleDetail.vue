8bd02ab61be76cd302d9342e539c88e6c6fa6479
silianpan <liupan6888@163.com>
2022-05-19 02:24:48 +0800

    迁移部分

--- src/views/example/components/ArticleDetail.vue
+++ src/views/example/components/ArticleDetail.vue
@@ -1,17 +1,14 @@
 <template>
   <div class="createPost-container">
     <el-form ref="postForm" :model="postForm" :rules="rules" class="form-container">
-
-      <sticky :z-index="10" :class-name="'sub-navbar '+postForm.status">
+      <sticky :z-index="10" :class-name="'sub-navbar ' + postForm.status">
         <CommentDropdown v-model="postForm.comment_disabled" />
         <PlatformDropdown v-model="postForm.platforms" />
         <SourceUrlDropdown v-model="postForm.source_uri" />
-        <el-button v-loading="loading" style="margin-left: 10px;" type="success" @click="submitForm">
+        <el-button v-loading="loading" style="margin-left: 10px" type="success" @click="submitForm">
           Publish
         </el-button>
-        <el-button v-loading="loading" type="warning" @click="draftForm">
-          Draft
-        </el-button>
+        <el-button v-loading="loading" type="warning" @click="draftForm"> Draft </el-button>
       </sticky>
 
       <div class="createPost-main-container">
@@ -19,25 +16,40 @@
           <Warning />
 
           <el-col :span="24">
-            <el-form-item style="margin-bottom: 40px;" prop="title">
-              <MDinput v-model="postForm.title" :maxlength="100" name="name" required>
-                Title
-              </MDinput>
+            <el-form-item style="margin-bottom: 40px" prop="title">
+              <MDinput v-model="postForm.title" :maxlength="100" name="name" required> Title </MDinput>
             </el-form-item>
 
             <div class="postInfo-container">
               <el-row>
                 <el-col :span="8">
                   <el-form-item label-width="60px" label="Author:" class="postInfo-container-item">
-                    <el-select v-model="postForm.author" :remote-method="getRemoteUserList" filterable default-first-option remote placeholder="Search user">
-                      <el-option v-for="(item,index) in userListOptions" :key="item+index" :label="item" :value="item" />
+                    <el-select
+                      v-model="postForm.author"
+                      :remote-method="getRemoteUserList"
+                      filterable
+                      default-first-option
+                      remote
+                      placeholder="Search user"
+                    >
+                      <el-option
+                        v-for="(item, index) in userListOptions"
+                        :key="item + index"
+                        :label="item"
+                        :value="item"
+                      />
                     </el-select>
                   </el-form-item>
                 </el-col>
 
                 <el-col :span="10">
                   <el-form-item label-width="120px" label="Publish Time:" class="postInfo-container-item">
-                    <el-date-picker v-model="displayTime" type="datetime" format="yyyy-MM-dd HH:mm:ss" placeholder="Select date and time" />
+                    <el-date-picker
+                      v-model="displayTime"
+                      type="datetime"
+                      format="yyyy-MM-dd HH:mm:ss"
+                      placeholder="Select date and time"
+                    />
                   </el-form-item>
                 </el-col>
 
@@ -49,7 +61,7 @@
                       :colors="['#99A9BF', '#F7BA2A', '#FF9900']"
                       :low-threshold="1"
                       :high-threshold="3"
-                      style="display:inline-block"
+                      style="display: inline-block"
                     />
                   </el-form-item>
                 </el-col>
@@ -58,16 +70,23 @@
           </el-col>
         </el-row>
 
-        <el-form-item style="margin-bottom: 40px;" label-width="70px" label="Summary:">
-          <el-input v-model="postForm.content_short" :rows="1" type="textarea" class="article-textarea" autosize placeholder="Please enter the content" />
+        <el-form-item style="margin-bottom: 40px" label-width="70px" label="Summary:">
+          <el-input
+            v-model="postForm.content_short"
+            :rows="1"
+            type="textarea"
+            class="article-textarea"
+            autosize
+            placeholder="Please enter the content"
+          />
           <span v-show="contentShortLength" class="word-counter">{{ contentShortLength }}words</span>
         </el-form-item>
 
-        <el-form-item prop="content" style="margin-bottom: 30px;">
+        <el-form-item prop="content" style="margin-bottom: 30px">
           <Tinymce ref="editor" v-model="postForm.content" :height="400" />
         </el-form-item>
 
-        <el-form-item prop="image_uri" style="margin-bottom: 30px;">
+        <el-form-item prop="image_uri" style="margin-bottom: 30px">
           <Upload v-model="postForm.image_uri" />
         </el-form-item>
       </div>
@@ -159,7 +178,7 @@ export default {
       // back end return => "2013-06-25 06:59:25"
       // front end need timestamp => 1372114765000
       get() {
-        return (+new Date(this.postForm.display_time))
+        return +new Date(this.postForm.display_time)
       },
       set(val) {
         this.postForm.display_time = new Date(val)
@@ -179,21 +198,23 @@ export default {
   },
   methods: {
     fetchData(id) {
-      fetchArticle(id).then(response => {
-        this.postForm = response.data
+      fetchArticle(id)
+        .then((response) => {
+          this.postForm = response.data
 
-        // just for test
-        this.postForm.title += `   Article Id:${this.postForm.id}`
-        this.postForm.content_short += `   Article Id:${this.postForm.id}`
+          // just for test
+          this.postForm.title += `   Article Id:${this.postForm.id}`
+          this.postForm.content_short += `   Article Id:${this.postForm.id}`
 
-        // set tagsview title
-        this.setTagsViewTitle()
+          // set tagsview title
+          this.setTagsViewTitle()
 
-        // set page title
-        this.setPageTitle()
-      }).catch(err => {
-        console.log(err)
-      })
+          // set page title
+          this.setPageTitle()
+        })
+        .catch((err) => {
+          console.log(err)
+        })
     },
     setTagsViewTitle() {
       const title = 'Edit Article'
@@ -206,7 +227,7 @@ export default {
     },
     submitForm() {
       console.log(this.postForm)
-      this.$refs.postForm.validate(valid => {
+      this.$refs.postForm.validate((valid) => {
         if (valid) {
           this.loading = true
           this.$notify({
@@ -240,9 +261,9 @@ export default {
       this.postForm.status = 'draft'
     },
     getRemoteUserList(query) {
-      searchUser(query).then(response => {
+      searchUser(query).then((response) => {
         if (!response.data.items) return
-        this.userListOptions = response.data.items.map(v => v.name)
+        this.userListOptions = response.data.items.map((v) => v.name)
       })
     }
   }
@@ -250,7 +271,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import "~@/styles/mixin.scss";
+@import '@/styles/mixin.scss';
 
 .createPost-container {
   position: relative;
---
+++ vite.config.js
@@ -0,0 +1,121 @@
+import path from 'path-browserify'
+// import fs from 'fs'
+import { defineConfig, loadEnv } from 'vite'
+import { createVuePlugin } from 'vite-plugin-vue2'
+import { createHtmlPlugin } from 'vite-plugin-html'
+import styleImport from 'vite-plugin-style-import'
+// import replace from '@rollup/plugin-replace'
+import eslintPlugin from 'vite-plugin-eslint'
+
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return defineConfig({
+    define: {
+      'process.env': { ...env }
+    },
+    plugins: [
+      createVuePlugin({
+        jsx: true
+      }),
+      eslintPlugin(),
+      createHtmlPlugin({
+        minify: true,
+        inject: {
+          data: {
+            title: 'vue Element Admin',
+            cdn: {
+              css: [],
+              js: [
+                // '//cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.min.js',
+                // '//cdn.jsdelivr.net/npm/vue-router@3.5.1/dist/vue-router.min.js',
+                // '//cdn.jsdelivr.net/npm/vuex@3.1.1/dist/vuex.min.js',
+                // '//cdn.jsdelivr.net/npm/axios@0.21.1/dist/axios.min.js',
+              ]
+            }
+          }
+        }
+      }),
+      styleImport({
+        libs: [
+          {
+            libraryName: 'element-ui',
+            // styleLibraryName: 'theme-chalk',
+            esModule: true,
+            resolveStyle: (name) => {
+              return `theme-chalk/${name}.css`
+            }
+          }
+        ]
+      })
+    ],
+    resolve: {
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+      alias: [
+        {
+          find: /@\/.+/,
+          replacement: (val) => {
+            return val.replace(/^@/, path.resolve(__dirname, './src/'))
+          }
+        },
+        {
+          // this is required for the SCSS modules
+          find: /^~(.*)$/,
+          replacement: '$1'
+        }
+        // {
+        //  find: '@',
+        //  replacement: path.resolve(__dirname, 'src'),
+        // },
+        // {
+        //   'venn.js': 'venn.js/build/venn.js',
+        // },
+      ]
+    },
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true,
+          // additionalData: `@import "./node_modules/ant-design-vue/lib/style/index.less";
+          //     @import "./node_modules/ant-design-vue/es/style/themes/default.less";`,
+          // additionalData: `@import "${path.resolve(__dirname, 'src/assets/style/global.less')}";`,
+          modifyVars: {}
+        }
+      }
+    },
+    build: {
+      target: 'es2015',
+      cssTarget: 'chrome80',
+      brotliSize: false,
+      chunkSizeWarningLimit: 2000,
+      commonjsOptions: {
+        //  改为 ture 后就会转化 require 语法
+        transformMixedEsModules: true
+      }
+      // rollupOptions: {
+      //   // 确保外部化处理那些你不想打包进库的依赖
+      //   external: ['vue', 'vue-router', 'vuex', 'axios', 'xlsx'],
+      //   output: {
+      //     // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+      //     globals: {
+      //       vue: 'Vue',
+      //       'vue-router': 'VueRouter',
+      //       vuex: 'Vuex',
+      //       axios: 'axios',
+      //       xlsx: 'XLSX'
+      //     }
+      //   }
+      // }
+    },
+    server: {
+      port: 9527
+      // proxy: {
+      //  '/api': {
+      //    target: 'https://mock.ihx.me/mock/5baf3052f7da7e07e04a5116/antd-pro',
+      //    changeOrigin: true,
+      //    ws: false,
+      //    rewrite: (path) => path.replace(/^\/api/, ''),
+      //  }
+      // },
+    }
+  })
+}
